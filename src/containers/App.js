6200af2f05bd3bcd573e14d132bec1eb2699bbdf
TV Quiz PhD <tvquizphd@gmail.com>
2021-04-26 02:06:29 -0400

    handle base url

--- src/containers/App.js
+++ src/containers/App.js
@@ -19,7 +19,7 @@ function RenderOutput({alignment}) {
 function App() {
 
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Switch>
           <Route path="/row">
